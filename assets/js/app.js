891bde56f2ec24103a86feca5873fe4412b48193
willakuqDev <willakuqdkrvision@gmail.com>
2021-12-21 16:50:32 -0500

    add of video tv transmision in live

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+function mostrarTv() {
+    let ventana =document.getElementById("ventanaTv");
+    let ventanaInicial=document.getElementById("ventanainicial");
+    let top=document.getElementById("top");
+    ventana.style.display="block";
+    ventanaInicial.style.display="none";
+    top.style.padding="0px";
+}
